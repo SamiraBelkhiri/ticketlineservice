4dc4aa1a73a8294f6fa26757e6c5f6b1247bf9ea
Victor-28 <victorakintola@gmail.com>
2020-03-10 16:52:34 +0100

    the end of the day

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
-
+die(phpinfo());
 require dirname(__DIR__).'/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
