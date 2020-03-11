c6ba4ab2b12811b1d6309310881f4b17a81c439f
becode <saliAlmurshidi@gmail.com>
2020-03-11 09:46:19 +0100

    add controller

--- src/Controller/MangerController.php
+++ src/Controller/MangerController.php
@@ -4,16 +4,37 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+session_start();
 class MangerController extends AbstractController
 {
     /**
      * @Route("/manger", name="manger")
      */
+
+
     public function index()
     {
+        $_SESSION["userID"] = 1;
+
+
         return $this->render('manger/index.html.twig', [
             'manager_name' => 'here we are',
         ]);
+
+
     }
-}
+
+   /* public function getManagerName (int $managerID){
+
+        /// connection with the db
+        $categorRepository = $this->getDoctrine()->getManagers()
+
+            $user = $categorRepository->getRepository('ticketlineservice\src\Entity\Users')->find($managerID);
+        return $this->render('',['categories'=>$user]);
+
+    }
+
+    public function
+
+    $dql = 'SELECT Users FROM App:category Users ORDER BY Users.userName DESC ';
+}*/
