416d9fc4703be14279bef3bec3b0be9441422095
Erick Medina <erickmedinabe@gmail.com>
2020-03-12 14:47:08 +0100

    fixing ticket

--- src/Entity/Tickets.php
+++ src/Entity/Tickets.php
@@ -79,6 +79,11 @@ class Tickets
      */
     private $comments;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
     public function __construct()
     {
         $this->comments = new ArrayCollection();
@@ -101,6 +106,7 @@ class Tickets
 
         return $this;
     }
+
 
     public function getOpenTime(): ?\DateTimeInterface
     {
@@ -249,6 +255,18 @@ class Tickets
                 $comment->setComment(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
