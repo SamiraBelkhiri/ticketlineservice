95a383a90705e26d2ffdfa2285389cfe8aa9256b
Tim-Smet <tim2smet@gmail.com>
2020-03-12 11:51:36 +0100

    Able to display all the ticket titles with status open

--- src/Controller/FLAgentController.php
+++ src/Controller/FLAgentController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Tickets;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TicketsRepository;
 
 class FLAgentController extends AbstractController
 {
     /**
-     * @Route("/f/l/agent", name="f_l_agent")
+     * @Route("/home/fl_agent", name="f_l_agent")
      */
     public function index()
     {
-        return $this->render('fl_agent/index.html.twig', [
-            'controller_name' => 'FLAgentController',
-        ]);
+        $ticket = $this->getDoctrine()->getRepository(Tickets::class)->findBy(['ticketStatus' => 'open']);
+        return $this->render('fl_agent/index.html.twig', array('ticket' => $ticket),
+        );
     }
+
 }
