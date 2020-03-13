9b2fe8b11d16a69a55ca443932ef9b652dee1990
Erick Medina <erickmedinabe@gmail.com>
2020-03-13 13:35:32 +0100

    added open tickets display and it works

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -25,33 +25,49 @@ class CustomerController extends AbstractController
         /** @var Tickets $ticket */
 
         $ticket = new Tickets();
-        $user = $this->getUser();
-        $form = $this->createForm(CreateTicketType::class, $ticket);
+        $user = $this -> getUser();
+        $form = $this -> createForm(CreateTicketType::class, $ticket);
 
-        $form->handleRequest($request);
+        $form -> handleRequest($request);
         //var_dump($form);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $ticket->setUser($user);
-            $ticket->setTicketTitle($form->getData()->getTicketTitle());
-            $ticket->setOpenTime(new \DateTime('@'.strtotime('now')));
-            $ticket->setCloseTime(new \DateTime('@'.strtotime('now')));
-            $ticket->setPriority('low');
-            $ticket->setTicketStatus('open');
-            $ticket->setAssignTo(0);
-            $ticket->setReOpen(0);
-            $ticket->setOpenTimee(new \DateTime('@'.strtotime('now')));
-            $ticket->setCloseTimee(new \DateTime('@'.strtotime('now')));
+        if ($form -> isSubmitted() && $form -> isValid()) {
+            $ticket -> setUser($user);
+            $ticket -> setTicketTitle($form -> getData() -> getTicketTitle());
+            $ticket -> setOpenTime(new \DateTime('@' . strtotime('now')));
+            $ticket -> setCloseTime(new \DateTime('@' . strtotime('now')));
+            $ticket -> setPriority('low');
+            $ticket -> setTicketStatus('open');
+            $ticket -> setAssignTo(0);
+            $ticket -> setReOpen(0);
+            $ticket -> setOpenTimee(new \DateTime('@' . strtotime('now')));
+            $ticket -> setCloseTimee(new \DateTime('@' . strtotime('now')));
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($ticket);
-            $entityManager->flush();
+            $entityManager = $this -> getDoctrine() -> getManager();
+            $entityManager -> persist($ticket);
+            $entityManager -> flush();
 
-            return $this->redirectToRoute('customer');
+            $this -> addFlash('success', 'Ticket created');
+            return $this -> redirectToRoute('customer');
         }
 
+        $userDetails = $this -> allOpenTickets($user);
 
-        return $this->render('customer/index.html.twig', [
-            'createTicket' => $form->createView(),
+        return $this -> render('customer/index.html.twig', [
+            'createTicket' => $form -> createView(),
+            'allTickets' => $userDetails
         ]);
+    }
+
+    public function allOpenTickets($userID): array
+    {
+        /** @var Users $user */
+        /** @var Tickets $ticket */
+
+        //$ticket = new Tickets();
+
+        $unSelectedTicket = $this -> getDoctrine() -> getRepository(Tickets::class) -> findBy(['user' => $userID]);
+
+        return $unSelectedTicket;
+
     }
 }
