aaf45edb606adfb285e2bd0db22ef92378a45017
Tim-Smet <tim2smet@gmail.com>
2020-03-13 12:34:32 +0100

    Done.

--- src/Controller/FLAgentController.php
+++ src/Controller/FLAgentController.php
@@ -3,20 +3,65 @@
 namespace App\Controller;
 
 use App\Entity\Tickets;
+use App\Entity\Users;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\TicketsRepository;
+use Symfony\Component\HttpFoundation\Session\Session;
+
 
 class FLAgentController extends AbstractController
 {
     /**
-     * @Route("/home/fl_agent", name="f_l_agent")
+     * @Route("/flagent", name="flagent")
      */
     public function index()
     {
-        $ticket = $this->getDoctrine()->getRepository(Tickets::class)->findBy(['ticketStatus' => 'open']);
-        return $this->render('fl_agent/index.html.twig', array('ticket' => $ticket),
-        );
+        /** @var Users $user */
+        $session = new Session();
+        $userName = $this->getUser()->getLastName();
+        $unSelectedTicket = $this->getDoctrine()->getRepository(Tickets::class)->findBy(['ticketStatus' => 'open']);
+        $SelectedTicket = $this->getDoctrine()->getRepository(Tickets::class)->findBy(['ticketStatus' => 'inProgress']);
+        if(isset($_POST['select'])){
+            $selectedTicket = $_POST['select'];
+            $selectedTicket = $this->getDoctrine()->getRepository(Tickets::class)->findBy(['id' =>$selectedTicket]);
+            $this->editStatusTicket($selectedTicket[0]);
+        }
+
+        return $this->render('fl_agent/index.html.twig', ['unSelectedTicket' => $unSelectedTicket,
+            'name' => $userName, 'selectedTicket' => $SelectedTicket
+            ]);
     }
+
+    /**
+     * @Route("/flagent/detail/{ticket}", name="detail")
+     * @param Tickets $ticket
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function TicketDetail (Tickets $ticket){
+
+        /**
+         * @var Tickets $thisTickets
+         */
+        $thisTickets= $this->getDoctrine()->getRepository(Tickets::class)->findOneBy(['id' => $ticket]);
+
+        $thisTickets->getAssignTo();
+
+        return $this->render('fl_agent/ticketDetail.html.twig', ['ticket' => $ticket]);
+    }
+
+    public function editStatusTicket($ticket)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $ticketObject = $em->getRepository(Tickets::class)->find($ticket);
+        if (!$ticketObject){
+            throw $this->createNotFOundException('No ticket found');
+        }
+        $ticketObject->setTicketStatus('inProgress');
+        $em->flush();
+       // $selectedTicket = $this->getDoctrine()->getRepository(Tickets::class)->findBy(['id' => $ticket]);
+    }
+
 
 }
---
+++ src/Migrations/Version20200313084520.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200313084520 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE tickets (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, ticket_title VARCHAR(255) NOT NULL, open_time DATE NOT NULL, close_time DATE DEFAULT NULL, priority VARCHAR(255) NOT NULL, assign_to INT DEFAULT NULL, re_open INT NOT NULL, ticket_status VARCHAR(255) NOT NULL, open_timee DATETIME NOT NULL, close_timee DATETIME DEFAULT NULL, description VARCHAR(255) NOT NULL, INDEX IDX_54469DF4A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE comments (id INT AUTO_INCREMENT NOT NULL, comment_id INT DEFAULT NULL, tickets_id INT NOT NULL, comments VARCHAR(500) DEFAULT NULL, date DATETIME NOT NULL, comment_status VARCHAR(255) NOT NULL, INDEX IDX_5F9E962AF8697D13 (comment_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, user_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE tickets ADD CONSTRAINT FK_54469DF4A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
+        $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962AF8697D13 FOREIGN KEY (comment_id) REFERENCES tickets (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962AF8697D13');
+        $this->addSql('ALTER TABLE tickets DROP FOREIGN KEY FK_54469DF4A76ED395');
+        $this->addSql('DROP TABLE tickets');
+        $this->addSql('DROP TABLE comments');
+        $this->addSql('DROP TABLE users');
+    }
+}
